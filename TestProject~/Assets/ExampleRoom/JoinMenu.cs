98c83a0d1c0c8b2342129bfea56294f1c7d4bf77
nickkhalow <nickhalow@gmail.com>
2025-07-24 13:56:40 +0300

    migrate to dcl lib

--- TestProject~/Assets/ExampleRoom/JoinMenu.cs
+++ TestProject~/Assets/ExampleRoom/JoinMenu.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using LiveKit;
 using UnityEngine.SceneManagement;
 
 public class JoinMenu : MonoBehaviour
@@ -9,7 +8,6 @@ public class JoinMenu : MonoBehaviour
     public static string LivekitURL { get; private set; }
     public static string RoomToken { get; private set; }
 
-    private LocalVideoTrack m_PreviewTrack;
     public RawImage PreviewCamera;
     public InputField URLField;
     public InputField TokenField;
@@ -27,26 +25,13 @@ public class JoinMenu : MonoBehaviour
             if (string.IsNullOrWhiteSpace(RoomToken))
                 return;
 
-            m_PreviewTrack?.Detach();
-            m_PreviewTrack?.Stop();
             SceneManager.LoadScene("RoomScene", LoadSceneMode.Single);
         });
     }
 
     private IEnumerator StartPreviewCamera()
     {
-        var f = Client.CreateLocalVideoTrack();
-        yield return f;
-
-        if (f.IsError)
-            yield break;
-
-        m_PreviewTrack = f.ResolveValue;
-
-        var video = m_PreviewTrack.Attach() as HTMLVideoElement;
-        video.VideoReceived += tex =>
-        {
-            PreviewCamera.texture = tex;
-        };
+        Debug.LogError("Preview camera is not supported");
+        yield break;
     }
 }
